daaf234b4d67956a4f6f5b0d926c2f8e392c9073
felipe <felipe@costacandido.com.br>
2018-05-13 16:13:15 -0300

    Atualizando

--- index.php
+++ index.php
@@ -3,56 +3,56 @@ require_once "vendor/autoload.php";
 
 
 
-/**
- * This example shows making an SMTP connection with authentication.
- */
-//Import the PHPMailer class into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-//SMTP needs accurate times, and the PHP time zone MUST be set
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
-//require '../vendor/autoload.php';
-//Create a new PHPMailer instance
-$mail = new PHPMailer;
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-//Set the hostname of the mail server
-$mail->Host = 'mail.costacandido.com.br';
-//Set the SMTP port number - likely to be 25, 465 or 587
-$mail->Port = 465;
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Username to use for SMTP authentication
-$mail->Username = 'sistemas@costacandido.com.br';
-//Password to use for SMTP authentication
-//sistemas@270910
-$mail->Password = 'sistemas@270910';
-//Set who the message is to be sent from
-$mail->setFrom('sistemas@costacandido.com.br', 'Felipe Silva');
-//Set an alternative reply-to address
-$mail->addReplyTo('felipe@costacandido.com.br', 'Felipe Silva');
-//Set who the message is to be sent to
-$mail->addAddress('felipe.candido8@gmail.com', 'Felipe Cândido');
-$mail->addAddress('vanessa@artesdaceci.com.br', 'Vanessa Silva');
-//Set the subject line
-$mail->Subject = 'Testando envio de email do novo sistema da Artes da celi';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-//Replace the plain text body with one created manually
-$mail->AltBody = 'Seu email não aceita HTML, por isso a mensagem não aparece!';
-//Anexos
-//$mail->addAttachment('images/phpmailer_mini.png');
-//send the message, check for errors
-if (!$mail->send()) {
-    echo 'Houve um erro ao enviar o email: ' . $mail->ErrorInfo;
-} else {
-    echo 'Mensagem enviada!';
-}
+	//include the RainTPL class
+	//include "inc/rain.tpl.class.php";
 
- ?>
+	raintpl::configure("base_url", null );
+	raintpl::configure("tpl_dir", "tpl/" );
+	raintpl::configure("cache_dir", "tmp/" );
+
+	//initialize a Rain TPL object
+	$tpl = new RainTPL;
+
+	//variable assign example
+	$variable = "Hello World!";
+	$tpl->assign( "variable", $variable );
+
+	//loop example
+	$week = array( 'Monday', 'Tuersday', 'Wednesday', 'Friday', 'Saturday', 'Sunday' );
+	$tpl->assign( "week", $week );
+
+	//loop example 2
+	$user = array(  array( 'name'=>'Jupiter', 'color'=>'yellow'),
+					array( 'name'=>'Mars', 'color'=>'red' ),
+					array( 'name'=>'Earth', 'color'=>'blue' ),
+	);
+	$tpl->assign( "user", $user );
+
+	//loop example with empty array
+	$tpl->assign( "empty_array", array() );
+
+	$info = array( 'title'=>'Rain TPL Example',
+                       'copyright' => 'Copyright 2006 - 2011 Rain TPL<br>Project By Rain Team' );
+
+	$tpl->assign( $info );
+
+	global $global_variable;
+	$global_variable = 'This is a global variable';
+
+
+        // you can draw the output
+        // $tpl->draw( 'page' );
+        // or the template output string by setting $return_string = true:
+	$html = $tpl->draw( 'page', $return_string = true );
+
+        // and then draw the output
+        echo $html;
+
+        
+        class str{
+            function cut($t){
+                return substr($t, 1, 2 );
+            }
+        }
+        
+?>
